e1a12fecc37907bb5513fc2d52e28e40e04c774f
Nick Kartashov <snailandmail@gmail.com>
2020-12-20 16:01:37 +0000

    Add 18 part 0: find border tiles

--- 2020/20/main.py
+++ 2020/20/main.py
@@ -4,6 +4,9 @@ from collections import defaultdict
 from copy import deepcopy
 from itertools import product
 from functools import lru_cache
+import random as rnd
+
+rnd.seed(42)
 
 def assrt(want, f, *args, **kwargs):
     got = f(*args, **kwargs)
@@ -20,7 +23,7 @@ def to_binary(code):
     def mapper(x):
         if x == '#':
             return '1'
-        return 0
+        return '0'
     return int(''.join(mapper(x) for x in code), 2)
 
 def flip(code):
@@ -52,7 +55,7 @@ class Tile(NamedTuple):
         return (to_binary(code) for code in (
             self.data[0],
             (row[-1] for row in self.data),
-            reversed(row[0] for row in self.data),
+            reversed([row[0] for row in self.data]),
             reversed(self.data[-1]),
         ))
 
@@ -71,15 +74,43 @@ def read_tiles(lines):
     return result
 
 
+def get_border_tiles(tiles):
+    horizontal_set = defaultdict(list)
+    for i, tile in enumerate(tiles):
+        for desc in generate_variants(tile.desc):
+            a, b, c, d = desc
+            horizontal_set[a].append(i)
+            horizontal_set[c].append(i)
+
+    outer_tile_idxs = set_sum([set(ms) for d, ms in horizontal_set.items() if len(ms) == 2])
+    return [tiles[idx] for idx in outer_tile_idxs]
+
+def read_test_tiles():
+    with open('test_tiles.txt') as infile:
+        lines = [line.strip() for line in infile.readlines()]
+        return read_tiles(lines)
+
+TEST_TILES = read_test_tiles()
+TEST_OUTER_TILE_IDS = {1951, 2311, 3079, 2473, 1171, 1489, 2971, 2729}
+
+def test_get_border_tiles():
+    actual = {tile.tile_id for tile in get_border_tiles(TEST_TILES)}
+    if actual != TEST_OUTER_TILE_IDS:
+        print(f"get_border_tiles returned {got}, expected {want}")
+
+test_get_border_tiles()
+
+def find_corner_tiles(border_tiles):
+    
+    pass
+	
+
 def main():
     lines = None
     with open('in.txt') as infile:
         lines = [line.strip() for line in infile.readlines()]
         tiles = read_tiles(lines)
-        print(len(tiles))
-        result = set()
-        for tile in tiles:
-            pass
+        border_tiles = get_border_tiles(tiles)
 
 
 
