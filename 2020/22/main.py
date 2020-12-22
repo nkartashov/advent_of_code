98252d502a6c0b48914c959997d2252a7e5f14c2
Nick Kartashov <snailandmail@gmail.com>
2020-12-22 17:38:38 +0000

    Add 22 part 2

--- 2020/22/main.py
+++ 2020/22/main.py
@@ -2,7 +2,7 @@ from typing import List, NamedTuple, Set, Tuple, Dict
 from enum import Enum
 from collections import defaultdict, deque
 from copy import deepcopy
-from itertools import product
+from itertools import product, islice
 from functools import lru_cache
 
 def assrt(want, f, *args, **kwargs):
@@ -40,6 +40,33 @@ def play(p1, p2):
 
 assrt(306, play, [9, 2, 6, 3, 1], [5, 8, 4, 7, 10])
 
+def play_recursive(p1, p2):
+    known_positions = set()
+    d1, d2 = deque(p1), deque(p2)
+
+    while d1 and d2:
+        key = tuple(d1), tuple(d2)
+        if key in known_positions:
+            return None, True
+        known_positions.add(key)
+
+        x, y = d1.popleft(), d2.popleft()
+        first_player_wins = x > y
+        if len(d1) >= x and len(d2) >= y:
+            _, first_player_wins = play_recursive(islice(d1, x), islice(d2, y))
+
+        if first_player_wins:
+            d1.extend([x, y])
+        else:
+            d2.extend([y, x])
+
+    first_player_wins = len(d1) > 0
+    if not first_player_wins:
+        return calculate_score(d2), first_player_wins
+
+    return calculate_score(d1), first_player_wins
+
+assrt((291, False), play_recursive, [9, 2, 6, 3, 1], [5, 8, 4, 7, 10])
 
 
 def main():
@@ -52,6 +79,7 @@ def main():
         p2 = [int(line.strip()) for line in infile.readlines()]
 
     print(play(p1, p2))
+    print(play_recursive(p1, p2)[0])
 
 
 
