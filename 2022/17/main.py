5df0bc413b3f13d852cde1072274870172da7529
Nick Kartashov <snailandmail@gmail.com>
2022-12-17 23:39:59 +0000

    Add 17/2022 part 2

--- 2022/17/main.py
+++ 2022/17/main.py
@@ -12,7 +12,7 @@ from typing import (
 )
 from enum import Enum
 from collections import deque
-from itertools import product, combinations
+from itertools import product, combinations, chain
 import traceback
 import tqdm
 
@@ -191,11 +191,93 @@ def solve1(lines: List[str], steps=2022) -> int:
 assrt(3068, solve1, TEST_DATA)
 
 
-def solve2(lines: List[str]) -> int:
+def flatten(l):
+    return list(chain.from_iterable(l))
+
+
+def serialize_glass(glass: List[List[bool]]) -> tuple:
+    assert len(glass) >= 100
+    return tuple(flatten(glass[-100:]))
+
+
+def solve2(lines: List[str], steps: int = 1_000_000_000_000) -> int:
+    cache = dict()
+
     jets = lines[0]
-    return simulate(jets, 1000_000_000_000)
+    # Row 0 is bottom.
+    glass = []
+    jet_counter = 0
+    result = 0
+
+    step = 0
+
+    with tqdm.tqdm(total=steps) as pbar:
+        while step != steps:
+            shape_idx = step % len(SHAPES)
+            jet_idx = jet_counter % len(jets)
+            caching_key = None, shape_idx, jet_idx
+
+            if len(glass) >= 100:
+                caching_key = serialize_glass(glass), shape_idx, jet_idx
+
+            if caching_key[0] is not None:
+                if caching_key in cache:
+                    (
+                        last_step_encountered,
+                        last_height_encountered,
+                    ) = cache[caching_key]
+                    step_diff = step - last_step_encountered
+                    height_diff = len(glass) - last_height_encountered
+                    steps_to_skip = steps - step
+                    times_skip = steps_to_skip // step_diff
+
+                    result += height_diff * times_skip
+                    step += step_diff * times_skip
+                    pbar.update(step_diff * times_skip)
+                else:
+                    cache[caching_key] = (
+                        step,
+                        len(glass),
+                    )
+
+            old_height = len(glass)
+            shape = SHAPES[shape_idx]
+
+            # Add 3 empty rows + empty rows for the shape.
+            for _ in range(3 + shape.height):
+                glass.append([False] * 7)
+
+            reference = [2, len(glass) - shape.height]
+            while True:
+                jet = jets[jet_counter % len(jets)]
+
+                # Jet pushes the rock.
+                if jet == LEFT:
+                    if shape.can_go_left(reference, glass):
+                        reference[0] -= 1
+                else:
+                    if shape.can_go_right(reference, glass):
+                        reference[0] += 1
+                jet_counter += 1
+
+                # Rock falls down.
+                if shape.can_go_down(reference, glass):
+                    reference[1] -= 1
+                else:
+                    shape.add(reference, glass)
+                    break
+
+            result += len(glass) - old_height
+            step += 1
+            pbar.update(step)
+
+        return result
 
 
+# assrt(15148, simulate, TEST_DATA[0], 10_000)
+assrt(15148, solve2, TEST_DATA, 10_000)
+# assrt(151434, simulate, TEST_DATA[0], 100_000)
+assrt(151434, solve2, TEST_DATA, 100_000)
 assrt(1514285714288, solve2, TEST_DATA)
 
 
@@ -203,6 +285,8 @@ def main():
     with open("in.txt") as infile:
         lines = [line.strip() for line in infile.readlines()]
         print(solve1(lines))
+        # assrt(150773, simulate, lines[0], 100_000)
+        assrt(150773, solve2, lines, 100_000)
         print(solve2(lines))
 
 
